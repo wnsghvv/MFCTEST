f0d727eb5b7158839cb00ff346392141a2edbf4a
wnsghvv <54825899+wnsghvv@users.noreply.github.com>
2019-12-17 11:40:13 +0900

    Add files via upload

---
+++ Fnt8/Fnt8Dlg.h
@@ -0,0 +1,36 @@
+﻿
+// Fnt8Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CFnt8Dlg 대화 상자
+class CFnt8Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CFnt8Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FNT8_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL PreTranslateMessage(MSG* pMsg);
+};
